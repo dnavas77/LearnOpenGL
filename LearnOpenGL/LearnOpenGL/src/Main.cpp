694d91d5103360f5ae2b61155c79a6ed89914606
dnavas77 <danilo.navas@outlook.com>
2018-02-26 23:39:47 -0500

    code reorganization

--- LearnOpenGL/LearnOpenGL/src/Main.cpp
+++ LearnOpenGL/LearnOpenGL/src/Main.cpp
@@ -1,16 +1,41 @@
-#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 void framebufferSizeCallback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+// shaders
+const char *fragmentShaderSource = R"FOO(
+	#version 460 core
+	out vec4 color;
+
+	void main()
+	{
+		color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+	}
+)FOO";
+
+const char* vertexShaderSource = R"FOO(
+	#version 460 core
+	layout (location = 0) in vec3 aPos;
+
+	void main()
+	{
+		gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
+	}
+)FOO";
+
+// main
 int main(void)
 {
-	GLFWwindow* window;
-
 	/* Initialize the library */
-	if (!glfwInit()) {
+	if (!glfwInit())
+	{
 		return -1;
 	}
 
@@ -18,60 +43,35 @@ int main(void)
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
-	if (!window) {
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment to compile on osx
+#endif
+
+	// GLFW window creation
+	//---------------------
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Learn OpenGL", NULL, NULL);
+	if (!window)
+	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
-	/* Initialize GLAD */
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+	// glad: load all OpenGL function pointers
+	// ---------------------------------------
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
 	}
 
-	/* Set OpenGL window rendering size */
-	glViewport(0, 0, 800, 600);
+	// Build and compile our shader program
+	// ------------------------------------
 
-	/* Register callback to resize OpenGL viewport when windows is resized */
-	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
-
-	/* Triangle Data & Setup */
-	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f,  0.5f, 0.0f
-	};
-	unsigned int VBO;
-	glGenBuffers(1, &VBO);
-
-	// Generate Vertex Array Object (VAO)
-	unsigned int VAO;
-	glGenVertexArrays(1, &VAO);
-	glBindVertexArray(VAO);
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-
-	// Set vertex data interpretation
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
-	glEnableVertexAttribArray(0);
-
-
-	/* Vertex shader */
-	const char* vertexShaderSource = R"FOO(
-		#version 460 core
-		layout (location = 0) in vec3 aPos;
-
-		void main() {
-			gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);
-		}
-	)FOO";
+	/* vertex shader */
 	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -80,28 +80,18 @@ int main(void)
 	int  success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
 	if (!success) {
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
 	}
 
 	/* Fragment shader */
-	const char* fragmentShaderSource = R"FOO(
-		#version 460 core
-		out vec4 color;
-
-		void main() {
-			color = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-		}
-	)FOO";
 	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
 	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
 	glCompileShader(fragmentShader);
 
 	// Error handling for shader compilation (glCompileShader)
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
 	if (!success) {
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
@@ -114,18 +104,52 @@ int main(void)
 	glLinkProgram(shaderProgram);
 
 	// Error handling for linking program (glLinkProgram)
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-
-	if (!success) {
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	glGetProgramiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
 	}
-
-	// Vertex deletion after linking with program
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	/* Loop until the user closes the window */
+	// Setup vertex data (and buffer(s)) and configure vertex attributes
+	float vertices[] = {
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f,  0.5f, 0.0f
+	};
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	// Bind the Vertex Array Object first, then bind and set vertex buffer(s)
+	// and the configure vertex attribute(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	// Note that this is allowed, the call to glVertexAttribPointer registered
+	// VBO as the vertex attribute's bound vertex buffer object so afterwards
+	// we can safely unbind
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify
+	// this VAO, but this rarely happens. Modifying other VAOs requires a call to
+	// glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when
+	// it's not directly necessary.
+	glBindVertexArray(0);
+
+	// Uncomment this call to draw in wireframe polygons.
+	//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+	// Render loop
+	// -----------
 	while (!glfwWindowShouldClose(window)) {
 		/* Process Input */
 		processInput(window);
@@ -134,31 +158,43 @@ int main(void)
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Draw our first object
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
 		glDrawArrays(GL_TRIANGLES, 0, 3);
 
-		/* Swap front and back buffers */
+		/* Swap front and back buffers and poll for IO events (keys, mouse, ect) */
 		glfwSwapBuffers(window);
-
-		/* Poll for and process events */
 		glfwPollEvents();
 	}
 
+	// Optional: deallocate all resources
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
+	// Terminate GLFW (clears all GLFW allocated resources)
 	glfwTerminate();
+
 	return 0;
 }
 
 
-/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
- * Function Definitions
- * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
-void framebufferSizeCallback(GLFWwindow* window, int width, int height) {
+// GLFW: whenever the window size is changed (by OS or user) this callback executes
+// --------------------------------------------------------------------------------
+void framebufferSizeCallback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and
+	// height will be significantly larger than specified on a retina displays.
 	glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow *window) {
-	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+// Process all input: query GLFW whether relevant keys are pressed/released
+// this frame and react accordingly
+// ------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
 		glfwSetWindowShouldClose(window, true);
 	}
 }
